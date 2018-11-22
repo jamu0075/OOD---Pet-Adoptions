36a98af58348102ea7512e9f3d327ab220435bf3
jamu0075 <jamu0075@colorado.edu>
2018-11-22 10:43:36 -0700

    Autodoc comments for Animals.py. index.rst used for auto commenting.

--- animals.py
+++ animals.py
@@ -1,47 +1,80 @@
 #!/usr/bin/env python3
+
 from abc import ABC, abstractmethod
 
 class PetInterface(ABC):
 
     @abstractmethod
     def get_Animal(self):
+        """Return the animal's type: str
+        """
         pass
 
     @abstractmethod
     def get_Species(self):
+        """Return the animal's species: str
+        """
         pass
 
     @abstractmethod
     def get_Name(self):
+        """Return the animal's name: str
+        """
         pass
 
     @abstractmethod
     def get_Gender(self):
+        """Return the animal's gender: str(M/F)
+        """
         pass
 
     @abstractmethod
     def get_Age(self):
+        """Return the animal's age: int
+        """
         pass
 
     @abstractmethod
     def get_Weight(self):
+        """Return the animal's weight: float
+        """
         pass
 
     @abstractmethod
     def get_ID(self):
+        """Return the animal's ID: int
+        """
         pass
 
     @abstractmethod
     def get_Status(self):
+        """Return the animal's status: str(Available, On-Hold, or Adopted)
+        """
         pass
 
     @abstractmethod
     def update_status(self, status):
+        """Updates the animal's status.
+
+        Args:
+            status: str(Available, On-Hold, or Adopted)
+        """
         pass
 
 class Dog(PetInterface):
 
     def __init__(self, species, name, gender, age, weight, id, isTrained):
+        """Information required to initialize a Dog. Initialize animal to 'Dog' and status to 'Available'
+
+            Args:
+                species(str): Breed of Dog\n
+                name(str): Name of Dog\n
+                gender(str): Gender of Dog(M/F)\n
+                age(int): Age of Dog\n
+                weight(float): Weight of Dog\n
+                id(int): Unique animal ID of Dog\n
+                isTrained(bool): Whether or not the Dog is house trained(True/False)
+        """
         self.animal = 'Dog'
         self.species = species
         self.name = name
@@ -77,6 +110,8 @@ class Dog(PetInterface):
         return self.status
 
     def get_IsTrained(self):
+        """Return the animal's house trained status: bool
+        """
         return self.isTrained
 
     def update_status(self, status):
@@ -85,6 +120,17 @@ class Dog(PetInterface):
 class Cat(PetInterface):
 
     def __init__(self, species, name, gender, age, weight, id, lifestyle):
+        """Information required to initialize a Cat. Initialize animal to 'Cat' and status to 'Available'
+
+            Args:
+                species: Breed of Cat\n
+                name: Name of Cat\n
+                gender: Gender of Cat\n
+                age: Age of Cat\n
+                weight: Weight of Cat\n
+                id: Unique animal ID of Cat\n
+                lifestyle: Whether the Cat lives indoors, outdoors, or both
+        """
         self.animal = 'Cat'
         self.species = species
         self.name = name
@@ -120,6 +166,8 @@ class Cat(PetInterface):
         return self.status
 
     def get_Lifestyle(self):
+        """Return the animal's preferred lifestyle(indoors, outdoors, or both)
+        """
         return self.lifestyle
 
     def update_status(self, status):
@@ -128,7 +176,17 @@ class Cat(PetInterface):
 class Bird(PetInterface):
 
     def __init__(self, species, name, gender, age, weight, id, lifestyle):
+        """Information required to initialize a Bird. Initialize animal to 'Bird' and status to 'Available'
 
+            Args:
+                species: Species of Bird\n
+                name: Name of Bird\n
+                gender: Gender of Bird\n
+                age: Age of Bird\n
+                weight: Weight of Bird\n
+                id: Unique animal ID of Bird\n
+                lifestyle: Whether the Bird lives caged, uncaged, or both
+        """
         self.animal = 'Bird'
         self.species = species
         self.name = name
@@ -164,6 +222,8 @@ class Bird(PetInterface):
         return self.status
 
     def get_Lifestyle(self):
+        """Return the animal's preferred lifestyle(caged, uncaged, or both)
+        """
         return self.lifestyle
 
     def update_status(self, status):
@@ -172,7 +232,17 @@ class Bird(PetInterface):
 class Reptile(PetInterface):
 
     def __init__(self, species, name, gender, age, weight, id, temperature):
+        """Information required to initialize a Reptile. Initialize animal to 'Reptile' and status to 'Available'
 
+            Args:
+                species: Species of Reptile\n
+                name: Name of Reptile\n
+                gender: Gender of Reptile\n
+                age: Age of Reptile\n
+                weight: Weight of Reptile\n
+                id: Unique animal ID of Reptile\n
+                temperature: The Reptile's preferred temperature
+        """
         self.animal = 'Reptile'
         self.species = species
         self.name = name
@@ -208,6 +278,8 @@ class Reptile(PetInterface):
         return self.status
 
     def get_Temperature(self):
+        """Return the animal's preferred temperature
+        """
         return self.temperature
 
     def update_status(self, status):
@@ -216,7 +288,16 @@ class Reptile(PetInterface):
 class Rabbit(PetInterface):
 
     def __init__(self, species, name, gender, age, weight, id):
+        """Information required to initialize a Rabbit. Initialize animal to 'Dog' and status to 'Available'
 
+            Args:
+                species: Breed of Rabbit\n
+                name: Name of Rabbit\n
+                gender: Gender of Rabbit\n
+                age: Age of Rabbit\n
+                weight: Weight of Rabbit\n
+                id: Unique animal ID of Rabbit\n
+        """
         self.animal = 'Rabbit'
         self.species = species
         self.name = name
---
+++ index.rst
@@ -0,0 +1,54 @@
+.. Pet Adoptions documentation master file, created by
+   sphinx-quickstart on Wed Nov 21 14:03:54 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+******************************************
+Welcome to Pet Adoptions's documentation!
+******************************************
+
+.. contents:: Table of Contents
+   :depth: 3
+
+.. section::
+
+Animal Classes
+===============
+All animal classes use the following interface:
+
+.. automodule:: animals
+   :members: PetInterface
+
+.. currentmodule:: animals
+
+Dog
+---
+.. autoclass:: Dog
+   :members:
+
+Cat
+---
+.. autoclass:: Cat
+   :members:
+
+Bird
+----
+.. autoclass:: Bird
+   :members:
+
+Reptile
+-------
+.. autoclass:: Reptile
+   :members:
+
+Rabbit
+------
+.. autoclass:: Rabbit
+   :members:
+
+-----------------------------------------------------------------
+
+Helper Functions
+=================
+
+-----------------------------------------------------------------
