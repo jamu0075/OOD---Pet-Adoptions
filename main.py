62b6796ba09d4847d337c44c23372542177a28f5
jamu0075 <jamu0075@colorado.edu>
2018-11-22 14:46:59 -0700

    main.py autodocumented.

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
-""" A Sphinx Test.
-.. Author: Jacob Munoz
-"""
 import pages
 import animals
 import helper
 
 #================================================================================================================================================
-#Factory that creates Pets
+
 class PetFactory:
+    """Factory Design Pattern that Creates Pet objects based on user inputted data
+    """
     def createPet(self, shelter):
 
         #Gets all required info from user
@@ -39,30 +38,55 @@ class PetFactory:
 #================================================================================================================================================
 #Shelter that manages Pets
 class Shelter:
+    """An object that keeps track of all the animals within a shelter. Holds a list of accepted animal types
+        along with a list of Pet objects to manage. The ID counter provides a unique ID for all new Pets.
+    """
     def __init__(self):
         self.animal_types = ['dog', 'cat', 'bird', 'rabbit', 'reptile']
         self.pet_directory = []
         self.id_counter = 0
 
     def get_ID(self):
+        """Returns current ID counter: int
+        """
         return self.id_counter
 
     def print_Pets(self):
+        """Prints all pets in the Pet Directory using helper.print_pet()
+        """
         for pet in self.pet_directory:
             helper.print_pet(pet)
 
     def print_Pets_Sorted(self, animal):
+        """Print all Pets that are an inputted animal type.
+
+            Args:
+                animal(str): animal type to sort by
+        """
         for pet in self.pet_directory:
             if pet.animal.lower() == animal:
                 helper.print_pet(pet)
 
     def increment_ID(self):
+        """Increment the ID to provide a unique ID
+        """
         self.id_counter = self.id_counter + 1
 
     def add_Pet(self, pet):
+        """Append a Pet to the Shelter's Pet Directory
+
+            Args:
+                pet(Pet): Pet object to add to the shelter
+        """
         self.pet_directory.append(pet)
 
     def update_Pet_Status(self, ID, status):
+        """Update a Pet's status
+
+            Args:
+                ID(int): Pet ID to update
+                status(str): new status for Pet
+        """
         for pet in self.pet_directory:
             if pet.id == ID:
                 pet.status = status
@@ -72,11 +96,10 @@ class Shelter:
 #Method that creates Pets for a default test shelter
 def defaultShelter(shelter):
     """THIS IS A TEST COMMENT ABOUT defaultShelter
-
     """
 
     shelter.increment_ID()
-    shelter.add_Pet(animals.Dog('German Shepard', 'Fido', 'M', 5, 40, shelter.get_ID(), 'Y'))
+    shelter.add_Pet(animals.Dog('German Shepard', 'Fido', 'M', 5, 40, shelter.get_ID(), True))
 
     shelter.increment_ID()
     shelter.add_Pet(animals.Cat('Mixed', 'Fluffy', 'F', 8, 5, shelter.get_ID(), 'Indoors'))
